2fb0e84c35c83a1cef37cb380ff09ccc5a806bee
Andrew Monterroso <amonterroso24@gmail.com>
2024-11-26 13:45:35 -0500

    update

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+var ReceiptDB = make(map[string]receipt)
+
 type item struct {
-	Description string  `json:"shortDescription`
-	Price       float64 `json:"price"`
+	Description string  `json:"shortDescription"`
+	Price       float64 `json:"price,string"`
 }
+
 type receipt struct {
 	Retailer string  `json:"retailer"`
 	Date     string  `json:"purchaseDate"`
 	Time     string  `json:"purchaseTime"`
-	Total    float64 `json:"total"`
+	Total    float64 `json:"total,string"`
 	Items    []item  `json:"items"`
 }
 
@@ -26,12 +33,27 @@ func getRecepit(c *gin.Context) {
 func processReceipts(c *gin.Context) {
 	var newReceipt receipt
 	if err := c.BindJSON(&newReceipt); err != nil {
+		c.JSON(http.StatusBadRequest, err)
 		return
 	}
 
+	//Marshal newReceipt so we can encode in SHA256
+	out, err := json.Marshal(newReceipt)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	h := sha1.New()
+	h.Write([]byte(out))
+	ReceiptID := hex.EncodeToString(h.Sum(nil))
+	fmt.Print(ReceiptID)
+	ReceiptDB[ReceiptID] = newReceipt
+
+	c.JSON(http.StatusOK, ReceiptID)
 }
 func main() {
 	router := gin.Default()
-	router.GET("/receipt", getRecepit)
+	router.GET("/receipt/:id", getRecepit)
+	router.POST("/receipts/process", processReceipts)
 	router.Run("localhost:8080")
 }
